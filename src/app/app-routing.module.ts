1f7c3f3554b3e94eb7063c68456e44528ed65d42
sidrockzz <marshtomp.1999@hotmail.com>
2021-04-23 12:40:49 +0530

    Routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {StudentsComponent} from './students/students.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path: 'students', component: StudentsComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports :[RouterModule]
 })
 export class AppRoutingModule { }
